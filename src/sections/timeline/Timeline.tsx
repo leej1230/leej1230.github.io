ab8b7bb5e06161249201cdec164cd25eccdefbf9
jaewoo <leejaewoo1230@gmail.com>
2023-07-07 18:04:58 +0900

    placeholder for timeline section

---
+++ src/sections/timeline/Timeline.tsx
@@ -0,0 +1,18 @@
+import { Container, Heading, Text } from '@chakra-ui/react'
+
+function Timeline() {
+  return (
+    <>
+        <Container minHeight="100vh" minWidth="100%" bg="blue.100">
+            <Heading>
+                This is Timeline Section
+            </Heading>
+            <Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nemo explicabo recusandae dignissimos beatae tempore vitae repellat amet facilis. Facilis ducimus suscipit in tenetur reprehenderit libero nisi ad nostrum vitae.
+            </Text>
+        </Container>
+    </>
+  )
+}
+
+export default Timeline
